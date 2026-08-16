8a453b96892d38eaf51dd8e39c00bfdef11c35cf
jeonck <jeonck2000@gmail.com>
2026-08-16 22:41:21 +0000

    Add Fox News listening content for 2026-08-17

--- sidebars.ts
+++ sidebars.ts
@@ -252,6 +252,7 @@ const sidebars: SidebarsConfig = {
                 'listening/fox-news/2026-08-14',
                 'listening/fox-news/2026-08-15',
                 'listening/fox-news/2026-08-16',
+                'listening/fox-news/2026-08-17',
               ],
             },
           ],
